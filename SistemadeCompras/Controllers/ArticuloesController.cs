b05f8d3897e5321fe34b90ca685348c3dab6e249
epf23 <40775303+epf23@users.noreply.github.com>
2019-10-29 23:17:15 -0400

    Revert "Revert "Agregando Controller de Articulo""
    This reverts commit 54333468616ce43c7f445eae5092cfc196e9be6f.

---
+++ SistemadeCompras/Controllers/ArticuloesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SistemadeCompras.Models;
+
+namespace SistemadeCompras.Controllers
+{
+    public class ArticuloesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Articuloes
+        public ActionResult Index()
+        {
+            return View(db.Articuloes.ToList());
+        }
+
+        // GET: Articuloes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articuloes.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // GET: Articuloes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Articuloes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descripcion,Marca,Id_Unidad_Medida,Existencia")] Articulo articulo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Articuloes.Add(articulo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(articulo);
+        }
+
+        // GET: Articuloes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articuloes.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // POST: Articuloes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descripcion,Marca,Id_Unidad_Medida,Existencia")] Articulo articulo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(articulo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(articulo);
+        }
+
+        // GET: Articuloes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articuloes.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // POST: Articuloes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Articulo articulo = db.Articuloes.Find(id);
+            db.Articuloes.Remove(articulo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
