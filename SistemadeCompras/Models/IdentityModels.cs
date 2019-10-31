eab971bd58ae4c402fcb0e580924cab8127fa442
Bass360 <erickpimentel1999@gmail.com>
2019-10-31 05:44:29 -0400

    Revert "CRUD Orden de Compras"
    This reverts commit fe66e438ac0135895bbcbcd91a2b86777e00162f.

--- SistemadeCompras/Models/IdentityModels.cs
+++ SistemadeCompras/Models/IdentityModels.cs
@@ -29,7 +29,5 @@ namespace SistemadeCompras.Models
         {
             return new ApplicationDbContext();
         }
-
-        public System.Data.Entity.DbSet<SistemadeCompras.Models.OrdenCompra> OrdenCompras { get; set; }
     }
 }
